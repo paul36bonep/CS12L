e51171bf085ecbcf255f2fa17b73b825e1066f77
Namae <148075468+paul36bonep@users.noreply.github.com>
2024-12-09 02:12:40 +0800

    Add files via upload

--- scts.sql
+++ scts.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 07, 2024 at 06:35 AM
+-- Host: 127.0.0.1:3307
+-- Generation Time: Dec 07, 2024 at 03:11 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,21 +28,79 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `agents` (
-  `agentID` int(6) NOT NULL,
-  `agentName` varchar(50) NOT NULL,
-  `area` varchar(50) NOT NULL,
-  `commissionPercent` float NOT NULL,
-  `username` varchar(21) NOT NULL,
-  `password` varchar(21) NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `AgentID` int(6) NOT NULL,
+  `AgentName` varchar(50) NOT NULL,
+  `Age` int(2) DEFAULT NULL,
+  `CommissionPercent` float NOT NULL,
+  `Area` varchar(50) NOT NULL,
+  `Status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `agents`
+-- Table structure for table `bank`
 --
 
-INSERT INTO `agents` (`agentID`, `agentName`, `area`, `commissionPercent`, `username`, `password`, `status`) VALUES
-(1, 'agentName', 'Davao City', 0.2, 'AgentA', '123', 1);
+CREATE TABLE `bank` (
+  `BankID` int(6) NOT NULL,
+  `BankName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `card`
+--
+
+CREATE TABLE `card` (
+  `CardID` int(6) NOT NULL,
+  `TypeID` int(6) NOT NULL,
+  `BankID` int(6) NOT NULL,
+  `Amount` float NOT NULL,
+  `Status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cardtype`
+--
+
+CREATE TABLE `cardtype` (
+  `TypeID` int(6) NOT NULL,
+  `CardType` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commissions`
+--
+
+CREATE TABLE `commissions` (
+  `CommissionID` int(6) NOT NULL,
+  `UserID` int(6) NOT NULL,
+  `AgentID` int(6) NOT NULL,
+  `TotalCommission` float NOT NULL,
+  `ApprovalStatus` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commissions_lines`
+--
+
+CREATE TABLE `commissions_lines` (
+  `Coms_Lines` int(6) NOT NULL,
+  `CommissionID` int(6) NOT NULL,
+  `CardID` int(6) NOT NULL,
+  `ClientName` varchar(50) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Amount` float NOT NULL,
+  `TotalAmount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -51,17 +109,9 @@ INSERT INTO `agents` (`agentID`, `agentName`, `area`, `commissionPercent`, `user
 --
 
 CREATE TABLE `positions` (
-  `positionID` int(6) NOT NULL,
-  `positionName` varchar(50) NOT NULL
+  `PositionID` int(6) NOT NULL,
+  `PositionName` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `positions`
---
-
-INSERT INTO `positions` (`positionID`, `positionName`) VALUES
-(1, 'Owner'),
-(2, 'Admin');
 
 -- --------------------------------------------------------
 
@@ -70,21 +120,13 @@ INSERT INTO `positions` (`positionID`, `positionName`) VALUES
 --
 
 CREATE TABLE `users` (
-  `userID` int(6) NOT NULL,
-  `positionID` int(6) NOT NULL,
-  `username` varchar(21) NOT NULL,
-  `password` varchar(21) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `UserID` int(6) NOT NULL,
+  `PositionID` int(6) NOT NULL,
+  `UserName` varchar(16) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userID`, `positionID`, `username`, `password`, `name`, `status`) VALUES
-(1, 1, 'SuperAdmin', '123', 'Owner', 1),
-(2, 2, 'Admin', '123', 'Admin_A', 1);
 
 --
 -- Indexes for dumped tables
@@ -94,52 +136,123 @@ INSERT INTO `users` (`userID`, `positionID`, `username`, `password`, `name`, `st
 -- Indexes for table `agents`
 --
 ALTER TABLE `agents`
-  ADD PRIMARY KEY (`agentID`);
+  ADD PRIMARY KEY (`AgentID`);
+
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`BankID`),
+  ADD UNIQUE KEY `BankName` (`BankName`);
+
+--
+-- Indexes for table `card`
+--
+ALTER TABLE `card`
+  ADD PRIMARY KEY (`CardID`),
+  ADD KEY `BankID` (`BankID`),
+  ADD KEY `TypeID` (`TypeID`);
+
+--
+-- Indexes for table `cardtype`
+--
+ALTER TABLE `cardtype`
+  ADD PRIMARY KEY (`TypeID`),
+  ADD UNIQUE KEY `CardType` (`CardType`);
+
+--
+-- Indexes for table `commissions`
+--
+ALTER TABLE `commissions`
+  ADD PRIMARY KEY (`CommissionID`),
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `AgentID` (`AgentID`);
+
+--
+-- Indexes for table `commissions_lines`
+--
+ALTER TABLE `commissions_lines`
+  ADD PRIMARY KEY (`Coms_Lines`),
+  ADD KEY `CommissionID` (`CommissionID`),
+  ADD KEY `CardID` (`CardID`);
 
 --
 -- Indexes for table `positions`
 --
 ALTER TABLE `positions`
-  ADD PRIMARY KEY (`positionID`);
+  ADD PRIMARY KEY (`PositionID`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`),
-  ADD KEY `positionID` (`positionID`);
+  ADD PRIMARY KEY (`UserID`),
+  ADD KEY `PositionID` (`PositionID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `agents`
+-- AUTO_INCREMENT for table `bank`
 --
-ALTER TABLE `agents`
-  MODIFY `agentID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `bank`
+  MODIFY `BankID` int(6) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `positions`
+-- AUTO_INCREMENT for table `cardtype`
 --
-ALTER TABLE `positions`
-  MODIFY `positionID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `cardtype`
+  MODIFY `TypeID` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `commissions`
+--
+ALTER TABLE `commissions`
+  MODIFY `CommissionID` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `commissions_lines`
+--
+ALTER TABLE `commissions_lines`
+  MODIFY `Coms_Lines` int(6) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `UserID` int(6) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `card`
+--
+ALTER TABLE `card`
+  ADD CONSTRAINT `card_ibfk_1` FOREIGN KEY (`BankID`) REFERENCES `bank` (`BankID`),
+  ADD CONSTRAINT `card_ibfk_2` FOREIGN KEY (`TypeID`) REFERENCES `cardtype` (`TypeID`);
+
+--
+-- Constraints for table `commissions`
+--
+ALTER TABLE `commissions`
+  ADD CONSTRAINT `commissions_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`),
+  ADD CONSTRAINT `commissions_ibfk_2` FOREIGN KEY (`AgentID`) REFERENCES `agents` (`AgentID`);
+
+--
+-- Constraints for table `commissions_lines`
+--
+ALTER TABLE `commissions_lines`
+  ADD CONSTRAINT `commissions_lines_ibfk_1` FOREIGN KEY (`CommissionID`) REFERENCES `commissions` (`CommissionID`),
+  ADD CONSTRAINT `commissions_lines_ibfk_2` FOREIGN KEY (`CardID`) REFERENCES `card` (`CardID`);
+
+--
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`positionID`) REFERENCES `positions` (`positionID`);
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`PositionID`) REFERENCES `positions` (`PositionID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
